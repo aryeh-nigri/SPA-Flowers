7d4633c1f0341b931da750fc75a2c794ce159de1
zeevNoiman <zevnoiman@gmail.com>
2019-03-24 00:00:29 +0300

    add branch
    finished task

--- SPA-Flowers/routes/branches.js
+++ SPA-Flowers/routes/branches.js
@@ -4,19 +4,11 @@ var router = express.Router();
 var branches = require('../models/')('Branch');
 
 /* GET branches listing. */
-router.get('/', async function (req, res) {
-    // Inquire all the users at once and get it as an array
-    try {
-        var branchesFromDB = await branches.REQUEST();
-        //console.dir(users, { showHidden: true, colors: true });
-        console.log(branchesFromDB);
-        res.render('partials/branches', {branches: branchesFromDB} )
-    } catch (err) { debug(`Failed: ${err}`) }
-});
-  
-  router.post('/', function (req, res) {
+router.get('/', renderBranches);
+
+router.post('/', async function (req, res) {
     var address = req.body.address;
-    var id = req.body.ID;
+    var id = req.body.id;
     var isActive;
     if(req.body.isActive)
       isActive = true;
@@ -25,15 +17,22 @@ router.get('/', async function (req, res) {
       
     console.log(address + id + isActive);
     
-    branches.CREATE([isActive, address, id]);
+    await branches.CREATE([isActive, address, id]);
 
-    console.log("req Base: " + req.baseUrl + "req original:" + req.originalUrl + "req url:" + req.url);
-    
-
-    var actualURL = req.originalUrl;
-    res.redirect(actualURL);
+    res.send({response: "CREATED"});
 
     // let branches = JSON.parse(fs.readFileSync('./database/branches.json', 'utf-8'));
     //res.render('partials/branches', {branches: branches} );
   });
+
+
+  async function renderBranches(req, res) {
+    // Inquire all the users at once and get it as an array
+    try {
+        var branchesFromDB = await branches.REQUEST();
+        //console.dir(users, { showHidden: true, colors: true });
+        console.log(branchesFromDB);
+        res.render('partials/branches', {branches: branchesFromDB} )
+    } catch (err) { debug(`Failed: ${err}`) }
+}
 module.exports = router;
